1755f06d25711ee87c430186d0653ec1e5c00aa1
simpzan <simpzan@gmail.com>
2013-06-02 11:13:33 +0800

    add benchmark for DACWrapper.

--- test/DACTest.cpp
+++ test/DACTest.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <gtest/gtest.h>
 #include "DACWrapper.h"
+#include "timer.h"
 
 using namespace std;
 
@@ -16,7 +17,7 @@ TEST(DACTest, test) {
   for (int i = 0; i < count; ++i) {
     EXPECT_EQ(int_array[i], dac[i]);
   }
-  
+
   stringstream ss;
   dac.write(ss);
   DACWrapper d2;
@@ -24,5 +25,37 @@ TEST(DACTest, test) {
   for (int i = 0; i < count; ++i) {
     EXPECT_EQ(int_array[i], d2[i]);
   }
+}
+
+TEST(DACTest, benchmark) {
+  int count = 1000000;
+  uint32_t int_array[count];
+  for (int i = 0; i < count; ++i) {
+    int_array[i] = i;
+  }
   
+  Timer timer;
+  DACWrapper dac(int_array, count);
+  timer.Stop();
+  cout << timer.ElapsedTimeCPU() << endl;
+
+  for (int i = 0; i < count; ++i) {
+    EXPECT_EQ(int_array[i], dac[i]);
+  }
+
+
+  timer.Restart();
+  for (int i = 0; i < count; ++i) {
+    int_array[i];
+  }
+  timer.Stop();
+  cout << timer.ElapsedTimeCPU() << endl;
+
+  timer.Restart();
+  for (int i = 0; i < count; ++i) {
+    dac[i];
+  }
+  timer.Stop();
+  cout << timer.ElapsedTimeCPU() << endl;
+
 }
